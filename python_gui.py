7214649a37359508722c8457bdc8e4f78eb670a4
Fredtonyvel <vfredanthony@yahoo.com>
2016-08-31 17:03:44 -0400

    Added code to change window size of widgets

--- python_gui.py
+++ python_gui.py
@@ -1,8 +1,22 @@
 from Tkinter import *
 
 root = Tk()
+
 theLabel = Label(root, text="I'll show you the dark side!")
 theLabel.pack()	#puts the contents of theLabel to display on gui screen
+
+#Learning about parameters 'fill' and 'expand' for pack() method
+#to make widget grow or shink in window size
+one = Label(root, text="One", bg="red", fg="white")
+one.pack()
+two = Label(root, text="Two", bg="green", fg="black")
+two.pack(fill=X)
+three = Label(root, text="Three", bg="blue", fg="white")
+three.pack(side=LEFT, fill=Y)
+four = Label(root, text="Four", bg="yellow", fg="black")
+four.pack(side=LEFT, fill=BOTH, expand=1)
+
+
 
 topFrame = Frame(root)
 topFrame.pack()
