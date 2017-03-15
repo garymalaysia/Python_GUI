2c1dfa01ff324b1ead7eb2b56d7590299efb6f43
fredtonyvel <vfredanthony@yahoo.com>
2017-03-15 00:37:17 -0400

    Finished practice script

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 
-def calculate():
+def calculate(*args):
 	try:
 		value = float(feet.get())
-		meters.set((0.3048 * value * 10000.0 + 0.5/10000.0))
+		meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
 	except ValuError:
 		pass
 
@@ -15,5 +15,24 @@ mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text='Calculate', command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text='feel').grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text='is equivalent to').grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text='meters').grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+	child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
 
 root.mainloop()
