eddf297a1b44d086c4bceef0f0acc090481275c4
fredtonyvel <vfredanthony@yahoo.com>
2017-03-15 13:26:00 -0400

    Finally figured out how to display user input on gui window instead of command line/terminal

--- Tkinter/projectGUI.py
+++ Tkinter/projectGUI.py
@@ -1,28 +1,38 @@
 from tkinter import *
 from tkinter import ttk
-from pythoncard import *
 
 class GUI:
 	def __init__(self, master):
 		self.master = master
 		master.title("JJAY Veterans Association HID Scanner")
+		master.update_idletasks()
+		width = 400
+		height = 400
+		x = (master.winfo_screenwidth() // 2) - (width // 2)
+		y = (master.winfo_screenheight() // 2) - (height // 2)
+		master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 		self.label = Label(master, text="GUI for HID Scanner")
 		self.label.pack()
 
-		self.greet_button = Button(master, text="Greet", command=self.greet)
-		self.greet_button.pack()
+		self.display = StringVar()
+		self.display_entry = ttk.Entry(master, width=7, textvariable=self.display)
+		self.display_entry.pack()
+
+		self.display_button = Button(master, text="Enter", command=self.displayMessage)
+		self.display_button.pack()
 
 		self.close_button = Button(master, text="Close", command=master.quit)
 		self.close_button.pack()
 
 
-	def greet(self, *args):
-		print("Greetings!")
-		message = 'Greetings'
-		gui.set(message)
 
+	def displayMessage(self):
+		self.displayText = self.display.get()
+		self.displayLabel = Label(self.master, text=self.displayText).pack()
+		return 
 
-root = Tk()
-my_gui = GUI(root)
-root.mainloop()
+if __name__ == "__main__":
+	root = Tk()
+	my_gui = GUI(root)
+	root.mainloop()
