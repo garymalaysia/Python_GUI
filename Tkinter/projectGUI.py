c47d9c8e21562e122d8a2ec8f009fb526618c045
fredtonyvel <vfredanthony@yahoo.com>
2017-03-15 16:43:01 -0400

    Organized layout and moved close button to lower right corner

--- Tkinter/projectGUI.py
+++ Tkinter/projectGUI.py
@@ -14,36 +14,24 @@ class GUI:
 		master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 		self.randLabel = Label(master, text="Number read: ")
-		self.randLabel.pack()
+		self.randLabel.grid(row=1, column=0, stick=W)
+		#self.randLabel.pack()
 		#self.randDisplay = StringVar()
 
 		self.display_button = Button(master, text="Random", command=self.randGen)
-		#self.display_button.grid(row=1, column=0, sticky = W)
-		self.display_button.pack()
+		self.display_button.grid(row=2, column=0, sticky=W)
+		#self.display_button.pack()
 
-		'''
-		self.label = Label(master, text="Enter a message: ")
-		#self.label.grid(row=0, sticky = W)
-		self.label.pack()
-
-		self.display = StringVar()
-		self.display_entry = ttk.Entry(master, width=40, textvariable=self.display)
-		#self.display_entry.grid(row=0, column=1)
-		self.display_entry.pack()
-
-		self.display_button = Button(master, text="Enter", command=self.displayMessage)
-		#self.display_button.grid(row=1, column=0, sticky = W)
-		self.display_button.pack()
-		'''
 		self.close_button = Button(master, text="Close", command=master.quit)
-		#self.close_button.grid(row=1, column=1, sticky = E)
-		self.close_button.pack()
+		#self.close_button.grid(row=10, column=5, sticky = E)
+		self.close_button.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
+		#self.close_button.pack()
 
 
 	def displayMessage(self):
 		self.displayText = self.display.get()
-		self.displayLabel = Label(self.master, text=self.displayText).pack()
-		#self.displayLabel.grid(row=3, stick= W)
+		self.displayLabel = Label(self.master, text=self.displayText)#.pack()
+		self.displayLabel.grid(row=8, stick= W)
 
 	def randGen(self):
 		self.a = random.randint(1,10)
@@ -54,17 +42,17 @@ class GUI:
 		
 	def EnterMessage(self):
 		self.label = Label(self.master, text="Enter a message: ")
-		#self.label.grid(row=0, sticky = W)
-		self.label.pack()
+		self.label.grid(row=5, sticky = W)
+		#self.label.pack()
 
 		self.display = StringVar()
 		self.display_entry = ttk.Entry(self.master, width=40, textvariable=self.display)
-		#self.display_entry.grid(row=0, column=1)
-		self.display_entry.pack()
+		self.display_entry.grid(row=5, column=1)
+		#self.display_entry.pack()
 
 		self.display_button = Button(self.master, text="Enter", command=self.displayMessage)
-		#self.display_button.grid(row=1, column=0, sticky = W)
-		self.display_button.pack()
+		self.display_button.grid(row=7, column=0, sticky = W)
+		#self.display_button.pack()
 
 if __name__ == "__main__":
 	root = Tk()
