38d22f667784760a76c63e2e39866325e4806a34
fredtonyvel <vfredanthony@yahoo.com>
2017-03-15 16:25:11 -0400

    Added function randGen to call function EnterMessage if rand is even #

--- Tkinter/projectGUI.py
+++ Tkinter/projectGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import random
 
 class GUI:
 	def __init__(self, master):
@@ -12,30 +13,58 @@ class GUI:
 		y = (master.winfo_screenheight() // 2) - (height // 2)
 		master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
+		self.randLabel = Label(master, text="Number read: ")
+		self.randLabel.pack()
+		#self.randDisplay = StringVar()
+
+		self.display_button = Button(master, text="Random", command=self.randGen)
+		#self.display_button.grid(row=1, column=0, sticky = W)
+		self.display_button.pack()
+
+		'''
 		self.label = Label(master, text="Enter a message: ")
-		self.label.grid(row=0, sticky = W)
-		#self.label.pack()
+		#self.label.grid(row=0, sticky = W)
+		self.label.pack()
 
 		self.display = StringVar()
 		self.display_entry = ttk.Entry(master, width=40, textvariable=self.display)
-		self.display_entry.grid(row=0, column=1)
-		#self.display_entry.pack()
+		#self.display_entry.grid(row=0, column=1)
+		self.display_entry.pack()
 
 		self.display_button = Button(master, text="Enter", command=self.displayMessage)
-		self.display_button.grid(row=1, column=0, sticky = W)
-		#self.display_button.pack()
-
+		#self.display_button.grid(row=1, column=0, sticky = W)
+		self.display_button.pack()
+		'''
 		self.close_button = Button(master, text="Close", command=master.quit)
-		self.close_button.grid(row=1, column=1, sticky = E)
-		#self.close_button.pack()
-
+		#self.close_button.grid(row=1, column=1, sticky = E)
+		self.close_button.pack()
 
 
 	def displayMessage(self):
 		self.displayText = self.display.get()
-		self.displayLabel = Label(self.master, text=self.displayText)
-		self.displayLabel.grid(row=3, stick= W)
-		return 
+		self.displayLabel = Label(self.master, text=self.displayText).pack()
+		#self.displayLabel.grid(row=3, stick= W)
+
+	def randGen(self):
+		self.a = random.randint(1,10)
+		if self.a%2 == 0:
+			#self.displayRand = Label(self.master, text=self.a).pack()
+			self.EnterMessage()
+		#print(self.a)
+		
+	def EnterMessage(self):
+		self.label = Label(self.master, text="Enter a message: ")
+		#self.label.grid(row=0, sticky = W)
+		self.label.pack()
+
+		self.display = StringVar()
+		self.display_entry = ttk.Entry(self.master, width=40, textvariable=self.display)
+		#self.display_entry.grid(row=0, column=1)
+		self.display_entry.pack()
+
+		self.display_button = Button(self.master, text="Enter", command=self.displayMessage)
+		#self.display_button.grid(row=1, column=0, sticky = W)
+		self.display_button.pack()
 
 if __name__ == "__main__":
 	root = Tk()
