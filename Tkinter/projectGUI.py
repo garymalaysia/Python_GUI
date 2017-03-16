3e1a6efbfd0191d268552d1bdcca95da7de0d19c
fredtonyvel <vfredanthony@yahoo.com>
2017-03-16 00:43:08 -0400

    Organized code and added comments, but need to improve bugs especially on resetting window

--- Tkinter/projectGUI.py
+++ Tkinter/projectGUI.py
@@ -13,49 +13,51 @@ class GUI:
 		y = (master.winfo_screenheight() // 2) - (height // 2)
 		master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
-		self.randLabel = Label(master, text="Number read: ")
-		self.randLabel.grid(row=1, column=0, stick=W)
-		#self.randLabel.pack()
-		#self.randDisplay = StringVar()
+		for x in range(1,10):
+			self.randButton()
+			self.closeButton()
+			self.clear()
+		
 
-		self.display_button = Button(master, text="Random", command=self.randGen)
-		self.display_button.grid(row=2, column=0, sticky=W)
+	#creates a button called 'Random'
+	def randButton(self):
+		self.display_button = Button(self.master, text="Random", command=self.randGen)
+		self.display_button.grid(row=1, sticky=W)
 		#self.display_button.pack()
 
-		self.close_button = Button(master, text="Close", command=master.quit)
+	#creates a button called 'Close'
+	def closeButton(self):
+		self.close_button = Button(self.master, text="Close", command=self.master.quit)
 		#self.close_button.grid(row=10, column=5, sticky = E)
 		self.close_button.place(rely=1.0, relx=0.0, x=0, y=0, anchor=SW)
 		#self.close_button.pack()
 
-
-	def displayMessage(self):
-		self.displayText = self.display.get()
-		self.displayLabel = Label(self.master, text=self.displayText)#.pack()
-		self.displayLabel.grid(row=8, stick= W)
-		self.display_button2 = Button(self.master, text="Random", command=self.randGen)
-		self.display_button2.grid(row=5, column=1, sticky=W)
-		self.clear()
-
+	#function that will call enterMessage function if rand_num is even
 	def randGen(self):
 		self.a = random.randint(1,10)
 		if self.a%2 == 0:
-			#self.displayRand = Label(self.master, text=self.a).pack()
 			self.enterMessage()
-		#print(self.a)
-		
+
+	#creates a bar to enter string and calls displayMessage function to display to window
 	def enterMessage(self):
 		self.label = Label(self.master, text="Enter a message: ")
-		self.label.grid(row=5, sticky = W)
+		self.label.grid(row=2, sticky = W)
 		#self.label.pack()
 
 		self.display = StringVar()
 		self.display_entry = ttk.Entry(self.master, width=40, textvariable=self.display)
-		self.display_entry.grid(row=5, column=1)
+		self.display_entry.grid(row=2, column=1)
 		#self.display_entry.pack()
 
 		self.display_button = Button(self.master, text="Enter", command=self.displayMessage)
-		self.display_button.grid(row=7, column=0, sticky = W)
+		self.display_button.grid(row=3, column=0, sticky = W)
 		#self.display_button.pack()
+
+	#function to display message to screen, not command line/terminal
+	def displayMessage(self):
+		self.displayText = self.display.get()
+		self.displayLabel = Label(self.master, text=self.displayText)#.pack()
+		self.displayLabel.grid(row=4, stick= W)
 
 	def clear(self):
 		self.label.grid_forget()
@@ -66,3 +68,8 @@ if __name__ == "__main__":
 	root = Tk()
 	my_gui = GUI(root)
 	root.mainloop()
+
+
+#Leftover pieces of code
+	#self.randLabel = Label(self.master, text="Click to activate generator")
+	#self.randLabel.grid(row=1, column=0, stick=W)
